c426010aea4ec4d415d8696f62069cc1e65f5f59
rajeshkumarui <115973571+rajeshkumarui@users.noreply.github.com>
2024-09-23 18:07:06 +0530

    Update index.js

--- itc-backend/index.js
+++ itc-backend/index.js
@@ -5,7 +5,11 @@ import mongoose from "mongoose";
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://grocery-client-phi.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 
 mongoose
   .connect(
