db07c47326283f23b5c0f27d76c00d6fa3a20ceb
rajeshkumarui <115973571+rajeshkumarui@users.noreply.github.com>
2024-09-23 18:43:05 +0530

    Update index.js

--- itc-backend/index.js
+++ itc-backend/index.js
@@ -7,7 +7,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors({
   origin: "https://grocery-client-phi.vercel.app",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["POST"],
   credentials: true,
   optionSuccessStatus:200,
 }));
